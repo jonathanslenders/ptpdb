ef149f3a62cdc689a666d3791c6e2324a56410a3
Jonathan Slenders <jonathan@slenders.be>
2015-04-23 15:39:26 +0100

    Pypy release 0.2

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 setup(
         name='ptpdb',
         author='Jonathan Slenders',
-        version='0.1',
+        version='0.2',
         url='https://github.com/jonathanslenders/ptpdb',
         description='Python debugger (pdb) build on top of prompt_toolkit',
         long_description='',
         packages=find_packages('.'),
         install_requires = [
-            'ptpython==0.5',
+            'ptpython==0.6',
         ],
 )
