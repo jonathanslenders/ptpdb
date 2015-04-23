3844b311f10b7542366baaf5172afb0f921199ad
Jonathan Slenders <jonathan@slenders.be>
2015-04-23 16:23:25 +0100

    Pypi release 0.3: fixed the last release which had wrong dependencies.

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup, find_packages
 setup(
         name='ptpdb',
         author='Jonathan Slenders',
-        version='0.2',
+        version='0.3',
         url='https://github.com/jonathanslenders/ptpdb',
         description='Python debugger (pdb) build on top of prompt_toolkit',
         long_description='',
         packages=find_packages('.'),
         install_requires = [
             'ptpython==0.6',
+            'prompt-toolkit==0.32',
         ],
 )
