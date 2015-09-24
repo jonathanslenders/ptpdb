3bcbe1a3af1db7b3e1f7ea6a75d6fc2ca5709c34
Jonathan Slenders <jonathan@slenders.be>
2015-09-24 04:53:44 +0200

    Pypi release 0.10.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 setup(
         name='ptpdb',
         author='Jonathan Slenders',
-        version='0.9',
+        version='0.10',
         url='https://github.com/jonathanslenders/ptpdb',
         description='Python debugger (pdb) build on top of prompt_toolkit',
         long_description='',
         packages=find_packages('.'),
         install_requires = [
-            'ptpython==0.22',
-            'prompt-toolkit==0.50',
+            'ptpython==0.23',
+            'prompt-toolkit==0.51',
         ],
 )
