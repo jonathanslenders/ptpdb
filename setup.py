896202b1c287fc628192918f901852ba739bb7e5
Jonathan Slenders <jonathan@slenders.be>
2016-01-03 21:30:14 +0100

    Release 0.12

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 
 setup(
-        name='ptpdb',
-        author='Jonathan Slenders',
-        version='0.11',
-        url='https://github.com/jonathanslenders/ptpdb',
-        description='Python debugger (pdb) build on top of prompt_toolkit',
-        long_description='',
-        packages=find_packages('.'),
-        install_requires = [
-            'ptpython==0.25',
-            'prompt-toolkit==0.54',
-        ],
+    name='ptpdb',
+    author='Jonathan Slenders',
+    version='0.12',
+    url='https://github.com/jonathanslenders/ptpdb',
+    description='Python debugger (pdb) build on top of prompt_toolkit',
+    long_description='',
+    packages=find_packages('.'),
+    install_requires = [
+        'ptpython==0.27',
+        'prompt-toolkit==0.56',
+    ],
 )
